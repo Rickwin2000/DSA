b7fdcd50c2d66c0e2fa709f9120889850e693854
Rickwin <rickwin.xavier@onedatasoftware.com>
2025-06-29 16:35:13 +0530

    feat: implement doubly and circular linked lists with insert, delete, and print functions

--- linked_list.py
+++ linked_list.py
@@ -3,6 +3,7 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prev = None
 
 class SingleLinkedList:
     def __init__(self):
@@ -63,14 +64,115 @@ class SingleLinkedList:
         while last:
             print("val_____", last.data)
             last = last.next  
-            
+    
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
 
-obj = SingleLinkedList()   
-obj.insert(10)
-obj.insert(20)
-obj.insert(30)
-obj.insert(40)
-obj.delete(10)
-print(obj.get(20))
+    def insert(self, data):
+        node = Node(data)
 
-obj.print_data()
+        if not self.head:
+            self.head = node
+            return
+        
+        last = self.head
+
+        while last.next:
+            last = last.next
+        
+        node.prev = last
+        last.next = node
+    
+    def delete(self, data):
+
+        if not self.head:
+            return
+
+        if self.head.data == data:
+            self.head = None
+        
+        last = self.head
+
+        while last:
+            if last.data == data:
+                previous = last.prev
+                next = last.next
+                
+                if next:
+                    next.prev = previous
+                if previous:
+                    previous.next = next
+
+            last = last.next
+    
+    def print_ln(self):
+        current = self.head
+
+        while current:
+            # print("current val_____ ", current.data)
+            if current.prev:
+                print("prev value_______ ", current.data, current.prev.data)
+            else:
+                print("prev value_______", current.data, None)
+            current = current.next
+
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            self.head.prev = node
+            self.head.next = node
+            return
+        
+        last = self.head.prev
+        last.next = node
+        node.prev = last
+        node.next = self.head
+        self.head.prev = node
+    
+    def print_ln(self):
+        current = self.head
+
+        if not self.head:
+            return
+
+        current = self.head
+        while True:
+            print(f"Node: {current.data}, Prev: {current.prev.data}, Next: {current.next.data}")
+            current = current.next
+            if current == self.head:
+                break
+
+
+def trigger_single_linked_list():
+    obj = SingleLinkedList()   
+    obj.insert(10)
+    obj.insert(20)
+    obj.insert(30)
+    obj.insert(40)
+    obj.delete(10)
+    print(obj.get(20))
+
+def trigger_doubly_linked_list():
+    obj = DoublyLinkedList()
+    obj.insert(10)
+    obj.insert(20)
+    obj.delete(20)
+    obj.insert(30)
+    obj.print_ln()
+
+def trigger_circular_linked_list():
+    obj = CircularLinkedList()
+    obj.insert(10)
+    obj.insert(20)
+    obj.insert(30)
+    obj.print_ln()
+
+trigger_circular_linked_list()
